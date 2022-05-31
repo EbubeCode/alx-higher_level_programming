dc5ad7b699bbe4169ef7202a36c155add1855a8e
EbubeCode <chukwuma258@gmail.com>
2022-05-31 17:17:30 +0100

    task 4

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if i != ord('e') and i != ord('q'):
-        print("{}".format(chr(i)), end='')
+for i in range(99):
+    print("{} = 0x{:x}".format(i, i))
