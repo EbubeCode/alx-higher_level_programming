db170b5e507fe0e30dd696c0e3f31e072636fda4
EbubeCode <chukwuma258@gmail.com>
2022-06-06 12:25:43 +0100

    0x03 task 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    new_list = []
+    if my_list:
+        new_list = my_list.copy()
+    if idx >= 0 and idx < len(new_list):
+        new_list[idx] = element
+    return new_list
