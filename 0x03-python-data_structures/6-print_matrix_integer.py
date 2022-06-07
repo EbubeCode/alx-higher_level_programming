5beb23c3bc09ca43e7703581f3224843e56593ba
EbubeCode <chukwuma258@gmail.com>
2022-06-07 19:41:26 +0100

    0x03 task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,11 +3,9 @@
 def print_matrix_integer(matrix=[[]]):
     if matrix:
         for lis in matrix:
-            if lis:
-                for i in range(len(lis)):
-                    end = ' '
-                    if i == len(lis) - 1:
-                        end = ''
-                    print("{:d}".format(lis[i]), end=end)
-
-                print()
+            for i in range(len(lis)):
+                end = ' '
+                if i == len(lis) - 1:
+                    end = ''
+                print("{:d}".format(lis[i]), end=end)
+            print()
