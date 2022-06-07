b92bd75607564ea0558ed3e61f869ccc82b4ec5f
EbubeCode <chukwuma258@gmail.com>
2022-06-07 19:26:30 +0100

    0x03 task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,8 +3,9 @@
 def print_matrix_integer(matrix=[[]]):
     if matrix:
         for lis in matrix:
-            for i in range(len(lis)):
-                end = ' '
-                if i == len(lis) - 1:
-                    end = '\n'
-                print("{:d}".format(lis[i]), end=end)
+            if lis:
+                for i in range(len(lis)):
+                    end = ' '
+                    if i == len(lis) - 1:
+                        end = '\n'
+                    print("{:d}".format(lis[i]), end=end)
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    if not len(my_list):
+    if not my_list:
         return None
     mx = 0
     for i in my_list:
