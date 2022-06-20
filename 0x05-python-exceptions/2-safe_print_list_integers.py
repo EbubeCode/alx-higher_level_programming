2422c1137203e618232100a282315fe83bd0a6ed
EbubeCode <chukwuma258@gmail.com>
2022-06-20 19:20:58 +0100

    0x04 task 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
 
+
+def safe_print_list_integers(my_list=[], x=0):
+    a = 0
+    b = 0
+    while a < x:
+        try:
+            print("{:d}".format(my_list[a]), end='')
+            a += 1
+            b += 1
+        except Exception as err:
+            if type(err) == IndexError:
+                raise
+            a += 1
+    print()
+    return b
