07a80e6face984bf3adfdbb689b1edc2636a6026
EbubeCode <chukwuma258@gmail.com>
2022-06-20 19:27:46 +0100

    0x04 task 3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def safe_print_division(a, b):
+    x = 0
+    try:
+        x = a / b
+    except Exception:
+        x = None
+    finally:
+        print("Inside result: {}".format(x))
+        return x
