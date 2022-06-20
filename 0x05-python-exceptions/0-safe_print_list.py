efa5e9eee1e916e569d180abd661535fe1b05724
EbubeCode <chukwuma258@gmail.com>
2022-06-20 18:53:41 +0100

    0x04 task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    a = 0
+    try:
+        for i in range(0, x):
+            print(f"{my_list[i]}", end='')
+            a += 1
+        print()
+        return a
+    except:
+        print()
+        return a
