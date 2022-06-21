1c25a762ab511bec95fbb7dc1436463230b53841
EbubeCode <chukwuma258@gmail.com>
2022-06-21 17:56:00 +0100

    0x06 task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""This module contains a Square class which has
+a private attribute __size"""
 
 
+class Square:
+    """A square class with the attribute __size"""
+    def __init__(self, size):
+        self.__size = size
