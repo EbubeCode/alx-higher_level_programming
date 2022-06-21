75eabb2c7659baf4dfa940ee04c927d05b39c6db
EbubeCode <chukwuma258@gmail.com>
2022-06-21 13:31:54 +0100

    0x04 task 8

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+from sys import stderr
+
 
 def safe_function(fct, *args):
     try:
         return fct(*args)
     except Exception as err:
-        print(f"Exception: {err}")
+        print(f"Exception: {err}", file=stderr)
         return None
