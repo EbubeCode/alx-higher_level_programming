91ab3b9f200efa85c6684236c91f4f0dcbedfc16
EbubeCode <chukwuma258@gmail.com>
2022-06-21 18:10:08 +0100

    0x06 task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This module contains a Square class which has
+a private attribute __size"""
+
+
+class Square:
+    """A square class with the attribute __size"""
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Calculates the area of the square"""
+        x = self.__size
+        return x * x
