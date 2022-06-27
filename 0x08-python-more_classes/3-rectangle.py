ad4e0809bef51f739f498772f86e1f2647754da8
EbubeCode <chukwuma258@gmail.com>
2022-06-27 13:43:55 +0100

    0x08 task 3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -15,7 +15,7 @@ class Rectangle:
         for i in range(self.__height):
             for j in range(self.__width):
                 strn += '#'
-            if i < self.__height - 1:
+            if i < self.__height - 1 and self.__width != 0:
                 strn += '\n'
         return strn
 
