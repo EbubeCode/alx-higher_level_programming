fea6507f8c75a2927a8fce045a2a58fcf97a9216
EbubeCode <chukwuma258@gmail.com>
2022-06-28 15:20:06 +0100

    0x07 task 2

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
-"""Defines a module that add two integers"""
+"""
+Defines a module that add two integers
+The module will be tested with doctest
+This test will be reference from a test file
+"""
+
 
 def add_integer(a, b=98):
-    """Adds two integers a and b, else raise exception"""
+    """
+    Adds two integers a and b, else raise exception
+    """
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
     return int(a) + int(b)
-
