716c07d94dcf99e301ef0fd01f8ffadaa29baae1
EbubeCode <chukwuma258@gmail.com>
2022-08-10 16:37:41 +0100

    0x0E task 14

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,17 @@
+-- query for table
+SELECT
+	g.name AS name
+FROM
+	tv_shows s
+INNER JOIN
+	tv_show_genres sg
+ON
+	s.id = sg.show_id
+INNER JOIN
+	tv_genres g
+ON
+	sg.genre_id = g.id
+WHERE
+	s.title = "Dexter"
+ORDER BY
+	g.name;
