67ec19fe658658f10a6ccdea5336de1aa70d608c
EbubeCode <chukwuma258@gmail.com>
2022-08-10 14:48:14 +0100

    0x0E task 12

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,15 @@
+-- join query
+SELECT
+	s.title AS title,
+	g.genre_id AS genre_id
+FROM
+	tv_shows s
+LEFT OUTER JOIN
+	tv_show_genres g
+ON
+	s.id = g.show_id
+WHERE
+	g.genre_id IS NULL
+ORDER BY
+	s.title,
+	genre_id;
