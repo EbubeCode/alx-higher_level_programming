a51c730049c2f219c8d792eed86b32018686982c
EbubeCode <chukwuma258@gmail.com>
2022-09-06 00:54:27 +0100

    0x12 task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!usr/bin/node
+const arglen = process.argv.length;
+if (arglen === 2) {
+  console.log('No argument');
+} else if (arglen === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
