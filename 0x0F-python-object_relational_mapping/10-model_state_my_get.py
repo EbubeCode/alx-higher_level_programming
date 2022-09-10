237863c33c8096f83e72dcf5c94436f397869a59
EbubeCode <chukwuma258@gmail.com>
2022-09-10 06:36:02 -0700

    ORM task 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''This module uses ORM to query a db'''
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    n = sys.argv[4]
+    s = session.query(State).filter(State.name == n).order_by(State.id).first()
+    if s:
+        print(f'{s.id}')
+    else:
+        print('Not found')
