a0bcaccfe740bf26b1fe8716ccfebb30d1e7dde3
EbubeCode <chukwuma258@gmail.com>
2022-09-09 19:25:04 -0700

    0RM task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''This script lists data in a database'''
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        exit(1)
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db = sys.argv[3]
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=username, passwd=password,
+                           db=db, charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        if row[1].startswith('N'):
+            print(row)
+    cur.close()
+    conn.close()
