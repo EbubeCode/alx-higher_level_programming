9f90f0fcc2fe05c930cf3a1583517ef09bb71ac7
EbubeCode <chukwuma258@gmail.com>
2022-09-10 03:40:32 -0700

    ORM task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''
+This script lists data in a database
+It takes an argument for searching
+'''
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    u = sys.argv[1]
+    p = sys.argv[2]
+    d = sys.argv[3]
+    conn = MySQLdb.connect(host="localhost", port=3306, user=u, passwd=p,
+                           db=d, charset="utf8")
+    cur = conn.cursor()
+    n = sys.argv[4]
+    cur.execute("SELECT * FROM states WHERE name=%s ORDER BY states.id ASC"
+                ,(n,))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
