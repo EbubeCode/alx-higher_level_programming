9bfcb939cbc1a8e80339c9fd71ad625b06416d77
EbubeCode <chukwuma258@gmail.com>
2022-09-23 02:31:52 +0100

    0x10 task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # query a header
 
-length=$(curl -sI "$1" | grep Content-Length | cut -d : -f2| sed -e 's/^[[:space:]]*//')
-echo "${length##*( )}"
+curl -sI "$1" | grep Content-Length | cut -d : -f2| sed -e 's/^[[:space:]]*//'
