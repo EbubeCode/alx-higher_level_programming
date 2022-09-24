ec5175f8d5a86db7ffd6fb4fd5ccc762d94ff565
EbubeCode <chukwuma258@gmail.com>
2022-09-24 04:56:49 +0100

    0x11 task 10

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,13 +7,13 @@ if __name__ == "__main__":
     owner = ''
     repo = ''
     if len(sys.argv) >= 2:
-        owner = sys.argv[1]
+        repo = sys.argv[1]
     if len(sys.argv) >= 3:
-        repo = sys.argv[2]
+        owner = sys.argv[2]
     url = f'https://api.github.com/repos/{owner}/{repo}/commits'
     res = requests.get(url)
     if res.status_code == 200:
-        json = res.json()
+        json = res.json()[:10]
         for c in json[:10]:
             name = c.get('commit').get('author').get('name')
             print(f"{c.get('sha')}: {name}")
