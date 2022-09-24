1fb1ac24fae068bac42598fe945116967c79735c
EbubeCode <chukwuma258@gmail.com>
2022-09-24 02:16:29 +0100

    0x11 task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# script that fetches https://alx-intranet.hbtn.io/status
+
+import urllib.request as request
+
+if __name__ == "__main__":
+    with request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        mess = res.read()
+        print('Body response:')
+        print(f"\t- type: {type(mess)}")
+        print(f"\t- content: {mess}")
+        print(f"\t- utf8 content: {mess.decode()}")
