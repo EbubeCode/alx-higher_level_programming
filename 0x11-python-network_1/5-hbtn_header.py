ed78cd2279b5442ced017b8a6c8c69bbec1b5965
EbubeCode <chukwuma258@gmail.com>
2022-09-24 03:13:39 +0100

    0x11 task 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 '''script that finds the X-Request-Id header for URL'''
-
 import requests
 import sys
 
 if __name__ == "__main__":
-    URL = sys.argv[1]
-    res = requests.get(URL)
-    h = res.headers['X-Request-Id']
-    print(h)
+    res = requests.get(sys.argv[1])
+    print(res.headers['X-Request-Id'])
